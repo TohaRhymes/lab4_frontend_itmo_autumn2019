b4b488855458d8f3b3e8b181411932b12e673baf
tohaRhymes <anton.chana@gmail.com>
2020-01-13 22:03:36 +0300

    front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from "react-router-dom";
+import "./static/styles/style.scss"
+import logo from "./static/img/vt-logo.png"
+import LoginPage from "./LoginPage/LoginPage";
+import MainPage from "./MainPage/MainPage";
+import RegisterPage from "./RegisterPage/RegisterPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Switch>
+                    <Route exact path="/login" component={LoginPage}/>
+                    <Route exact path="/register" component={RegisterPage}/>
+                    <Route path="/" component={MainPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
+
+
+const Header = () => {
+    return (<div className="head">
+        <span id="title">
+            Web-project #4. Variant 21911
+            <p><a href="https://github.com/TohaRhymes" target="_blank" rel="noopener noreferrer">Changalidi Anton, P3214</a>
+            </p>
+            </span>
+
+        <img  className="App-logo" src={logo} alt="logo"/>
+        {/*<a href="http://se.ifmo.ru" target="_blank" rel="noopener noreferrer"> <img  className="App-logo" src={logo} alt="logo"/></a><br/>*/}
+
+    </div>)
+};
+
 
 export default App;
